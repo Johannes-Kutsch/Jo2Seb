51d4934fce4470ceeb045dd841e9dd8a5f7597d6
Johannes Kutsch <johanneskutsch@live.de>
2026-03-10 13:34:58 +0100

    * first baseline model (use mean of whole data set)

--- python/Utils/FilePaths.py
+++ python/Utils/FilePaths.py
@@ -3,5 +3,8 @@
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 GFW = PROJECT_ROOT / "files/data/lfs/GFW"
 DWD = PROJECT_ROOT / "files/data/lfs/DWD"
+UWB = PROJECT_ROOT / "files/data/lfs/UWB"
+UWB_DATA = UWB / "data_tables"
+UWB_METADATA = UWB / "metadata"
 VOYAGES_DIR = GFW / "voyages"
 WEATHER_DIR = DWD / "weather"
