d7823d195a73bba0fe458381c42e6d30a288b942
johannes <johannes@allweier-online.de>
2026-03-18 10:51:21 +0100

    added variable path for Pegel

--- python/Utils/FilePaths.py
+++ python/Utils/FilePaths.py
@@ -4,6 +4,7 @@ PROJECT_ROOT = Path(__file__).parent.parent.parent
 GFW = PROJECT_ROOT / "files/data/lfs/GFW"
 DWD = PROJECT_ROOT / "files/data/lfs/DWD"
 UWB = PROJECT_ROOT / "files/data/lfs/UWB"
+PEGEL = PROJECT_ROOT / "files/data/lfs/Pegel"
 COPERNICUS = PROJECT_ROOT / "files/data/lfs/Copernicus"
 UWB_DATA = UWB / "data_tables"
 UWB_METADATA = UWB / "metadata"
