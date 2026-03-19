9592ed1c99447f7cd8ad3742d48e962fbb5edeba
johannes <johannes@allweier-online.de>
2026-03-19 11:43:33 +0100

    added windpipe

--- python/Pipeline/PipelienFactory.py
+++ python/Pipeline/PipelienFactory.py
@@ -10,12 +10,23 @@ from Pipeline.Transformers.TemporalFeatureBuilder import TemporalFeatureBuilder
 
 class PipelineFactory:
     @staticmethod
-    def create_main_pipeline(pm10_params = None):
+    def create_main_pipeline(pm10_params = None, wind_params = None):
         pm10_pipeline, _ = PipelineFactory.create_pm_pipeline(pm10_params)
+        wind_pipeline, _ = PipelineFactory.create_windpipe(wind_params)
 
         main_pipeline = Pipeline([
+            ### Airpolution 
             ("pm10_pipeline", pm10_pipeline),
-            ("drop_head_nan", DropHeadNaN())
+
+            ## Weather
+            ("wind_pipe", wind_pipeline),
+            ("drop_unused_weather_features", FeatureDropTransformer(['precipitation', 'pressure_msl', 'sunshine', 'temperature',
+            'cloud_cover', 'dew_point', 'relative_humidity', 'visibility', 'solar']) ),
+
+            ## Put as last pipeline
+            ("drop_head_nan", DropHeadNaN()),  
+   
+            
         ])
 
         return main_pipeline
@@ -40,6 +51,25 @@ class PipelineFactory:
             ("temporal_feature_builder", TemporalFeatureBuilder("PM10", lags=params["pm10_lags"], rolling_windows=params["pm10_rolling_windows"],)),
             ("residual_temporal_feature_builder", TemporalFeatureBuilder("PM10_residuals", lags=params["residual_lags"], rolling_windows=params["residual_rolling_windows"])),
             ("drop_PM_10", FeatureDropTransformer(["PM10_residuals", "PM10"])),
+        ])
+
+
+        return pipeline, params
+    
+    @staticmethod
+
+    def create_windpipe(params: dict = None):
+        if params is None:
+            params = {
+                "seasonal_periods": [365],
+                "trend_model": LinearRegression(),
+                "lags": [1, 2, 3, 4, 7, 8, 14, 15, 21, 22, 28, 29],
+                "rolling_windows": [2, 3, 4],
+            }
+        
+        pipeline = Pipeline([
+            #("mstl_decomposer", MSTLDecomposer(column_name='sunshine', seasonal_periods=params["seasonal_periods"], trend_model= params["trend_model"])),
+            ("temporal_features", TemporalFeatureBuilder(['w_x', 'w_y', 'wg_x', 'wg_y'], lags=params["lags"], rolling_windows=params["rolling_windows"],  drop_original=True)),
         ])
 
         return pipeline, params
